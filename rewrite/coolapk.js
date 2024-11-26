1c4c2bd678cfa77a4857d9506ffd2232f89c838d
Z <155460219+z-jinke@users.noreply.github.com>
2024-11-26 19:08:11 +0800

    Create coolapk.js

---
+++ rewrite/coolapk.js
@@ -0,0 +1,8 @@
+hostname = *.coolapk.com
+
+^https?:\/\/api\.coolapk\.com\/v6\/main\/init url script-response-body https://github.com/z-jinke/quanx/raw/refs/heads/main/scripts/coolapk.js
+^https?:\/\/api\.coolapk\.com\/v6\/main\/indexV8 url script-response-body https://github.com/z-jinke/quanx/raw/refs/heads/main/scripts/coolapk.js
+^https?:\/\/api\.coolapk\.com\/v6\/search url script-response-body https://github.com/z-jinke/quanx/raw/refs/heads/main/scripts/coolapk.js
+^https?:\/\/api\.coolapk\.com\/v6\/page url script-response-body https://github.com/z-jinke/quanx/raw/refs/heads/main/scripts/coolapk.js
+^https?:\/\/api\.coolapk\.com\/v6\/feed url script-response-body https://github.com/z-jinke/quanx/raw/refs/heads/main/scripts/coolapk.js
+^https?:\/\/api\.coolapk\.com\/v6\/account\/loadConfig url script-response-body https://github.com/z-jinke/quanx/raw/refs/heads/main/scripts/coolapk.js
